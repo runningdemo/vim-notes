d419f6137fd2930ae17048c8811b9ebfd19da35c
liang chen <demo+github@workstream.is>
2022-10-15 01:24:09 +0800

    Handle special charaters

--- lua/notes/planning-module.lua
+++ lua/notes/planning-module.lua
@@ -1,13 +1,16 @@
 local function insertTodayTitle()
     -- 2022-10-14
-    todayString = os.date("%Y-%m-%d")
+    todayString = os.date("## %Y-%m-%d")
+    print(todayString)
     vim.api.nvim_command('exec "normal 7G"')
     lineContent = vim.api.nvim_get_current_line()
-    if string.match(lineContent, todayString) then
+    print(lineContent, todayString)
+    if string.find(lineContent, todayString, 1, true) then
       print ("The word tiger was found.")
     else
+      print ("not match")
       vim.api.nvim_command('exec "normal O"')
-      vim.api.nvim_set_current_line(os.date("## %Y-%m-%d"))
+      vim.api.nvim_set_current_line(todayString)
     end
   end
 
